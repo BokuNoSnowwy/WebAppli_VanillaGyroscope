f061ac90726eff25a71f41482474ffca00d7a992
Mornet Valentin <snow855@hotmail.fr>
2021-01-11 11:48:18 +0100

    TestLogo

--- js/game.js
+++ js/game.js
@@ -1,8 +1,18 @@
 const canvas = document.getElementById('canvasTest');
 const ctx = canvas.getContext('2d');
 
-let x = 50
-let y = 10
+canvas.width = document.documentElement.clientWidth
+
+
+let rect = {
+    x = 50,
+    y = 10,
+    w = 150,
+    h = 100,
+    color : 'red',
+    dx = 2,
+    dy = 2
+}
 
 let xAdd = 1
 let yAdd = 1
@@ -10,56 +20,51 @@ let yAdd = 1
 let frame = 0
 let variable = "Hello"
 
+var img = new Image();
+img.src = 'DVD_video_logo.pnj'
+
+
+
+
 function gameLoop(){
-
-
-
-    let firstTime
 
     let widthRectangle = 100
     let heighRectangle = 100
 
-    let bounceRight = true;
-    let bounceDown = true
 
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'green';
     ctx.fillRect(0, 0, canvas.width, canvas.height); 
+    
+    //ctx.drawImage(img, x,y, widthRectangle,heighRectangle)
     
     ctx.fillStyle = 'red';
     ctx.fillRect(x, y, widthRectangle, heighRectangle); 
-
-
+    
 
         if(x + widthRectangle > canvas.width){
             xAdd = -1
-            bounceRight = false
-            console.log("BounceRight")
         }
 
         if(x < 0){
             xAdd = 1
-            bounceRight = true
-            console.log("BounceLeft")
         }
 
-  
         if(y + heighRectangle > canvas.height){
             yAdd = -1
-            bounceDown = false
-            console.log("BounceDown")
         }
 
         if(y < 0){
             yAdd = 1
-            bounceDown = true
-            console.log("BounceUp")
         }
     
 
     x += xAdd
     y += yAdd
+
+}
+
+function rectDraw(){
     
-       
 }
 
 setInterval(gameLoop,1000 /60)
