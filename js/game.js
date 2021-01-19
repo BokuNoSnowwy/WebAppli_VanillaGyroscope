64563f0888ff2ef1fc119311107748caacc5f92c
Mornet Valentin <snow855@hotmail.fr>
2021-01-19 14:31:14 +0100

    GyroTest

--- js/game.js
+++ js/game.js
@@ -2,7 +2,11 @@ const canvas = document.getElementById('canvasTest');
 const ctx = canvas.getContext('2d');
 
 canvas.width = document.documentElement.clientWidth || document.body.clientWidth;
-canvas.height = document.documentElement.clientHeight || document.body.clientHeight;
+canvas.height = document.documentElement.clientHeight - 60|| document.body.clientHeight - 60;
+
+let debugText = new Text();
+debugText = document.getElementById('text');
+
 
 function rect_create(x,y,w,h,color,dx,dy){
     let obj = {
@@ -18,12 +22,20 @@ function rect_create(x,y,w,h,color,dx,dy){
     return obj
 }
 
-let rect = rect_create(10,20,30,50,'red',3,3)
-let rect2 = rect_create(100,20,30,50,'blue',2,3)
+//let rect = rect_create(10,20,30,50,'red',3,3)
+//let rect2 = rect_create(100,20,30,50,'blue',2,3)
 
 let gameobjects = [
-    rect,
-    rect2
+    //rect,
+    //rect2
+]
+
+let colorsDrawing = [
+    "#ffbe0b",
+    "#fb5607",
+    "#ff006e",
+    "#8338ec",
+    "#3a86ff"
 ]
 
 let frame = 0
@@ -31,12 +43,43 @@ let frame = 0
 var img = new Image();
 //img.src = 'DVD_video_logo.pnj'
 
+let gyroValue = {
+    x: 0,
+    y: 0,
+    z: 0
+}
+
+let gyroscope = new Gyroscope({frequency: 60});
+
+gyroscope.addEventListener('reading', e => {
+  gyroValue.x += gyroscope.x;
+  gyroValue.y += gyroscope.y;
+  gyroValue.z += gyroscope.z;
+  document.getElementById("gyro").innerHTML = Math.floor(gyroValue.x) + "<br>" + Math.floor(gyroValue.y) + "<br>" + Math.floor(gyroValue.z)
+});
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+function getRandomBetween2Values(min, max) 
+{
+    return Math.floor(Math.random() * (+max + 1 - +min)) + +min; 
+}
 
 function gameLoop(){
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvas.width, canvas.height); 
     
     //ctx.drawImage(img, x,y, widthRectangle,heighRectangle)
+    debugText.textContent = "Debug console : ";
+    debugText.textContent += "xGyro : " + Math.round(gyroValue.x) + "  yGyro : " + Math.round(gyroValue.y) + "  zGyro : " + Math.round(gyroValue.z);
+
+    console.log(gyroValue.x);
+    console.log(gyroValue.y);
+    console.log(gyroValue.z);
+
 
     gameobjects.forEach((obj) => obj.draw())
 }
@@ -77,13 +120,7 @@ acl.addEventListener('reading', () => {
   //gameLoop
 });
 
-let gyroscope = new Gyroscope({frequency: 60});
 
-gyroscope.addEventListener('reading', e => {
-  console.log("Angular velocity along the X-axis " + gyroscope.x);
-  console.log("Angular velocity along the Y-axis " + gyroscope.y);
-  console.log("Angular velocity along the Z-axis " + gyroscope.z);
-});
 
 
 function startup() {
@@ -96,37 +133,25 @@ function startup() {
   document.addEventListener("DOMContentLoaded", startup);
 
 function handleStart(evt) {
-    let rect2 = rect_create(100,20,30,50,'blue',2,3);
-    gameobjects.push(rect2);
+   
     evt.preventDefault();
-    console.log("touchstart.");
+
+    let dxRect = getRandomBetween2Values(-2, 2);
+    let dyRect = getRandomBetween2Values(-2, 2);
+
+    let xSize = getRandomBetween2Values(10, 200);
+    let ySize = getRandomBetween2Values(10, 200);
+
+    let color = colorsDrawing[getRandomInt(colorsDrawing.length)];
+    
+
+    let rect = rect_create(evt.x,evt.y,xSize,ySize,color,dxRect,dyRect);
+    gameobjects.push(rect);
 }
   
 function handleMove(evt) {
     evt.preventDefault();
     var touches = evt.changedTouches;
-
-    for (var i = 0; i < touches.length; i++) {
-        var color = colorForTouch(touches[i]);
-        var idx = ongoingTouchIndexById(touches[i].identifier);
-
-        if (idx >= 0) {
-        console.log("continuing touch "+idx);
-        ctx.beginPath();
-        console.log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
-        ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-        console.log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
-        ctx.lineTo(touches[i].pageX, touches[i].pageY);
-        ctx.lineWidth = 4;
-        ctx.strokeStyle = color;
-        ctx.stroke();
-
-        ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-        console.log(".");
-        } else {
-        console.log("can't figure out which touch to continue");
-        }
-    }
 }
 
 gyroscope.start();
